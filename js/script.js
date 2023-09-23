2aa9e04a65a36261b30cade55c9d3d44a31052e7
Santoshranawat <santosh73799@gmail.com>
2023-09-23 10:50:58 +0530

    a

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
  // splide slider Scripts
  const splide = new Splide('.splide', {
     type: 'loop',
@@ -35,3 +34,24 @@ function billPayment(evt, billpay) {
     evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+
+ // Get the button
+ let mybutton = document.getElementById("myBtn");
+        
+ // When the user scrolls down 20px from the top of the document, show the button
+ window.onscroll = function() {scrollFunction()};
+ 
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+ 
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
